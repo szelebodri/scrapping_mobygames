7dace352e9a81afcdedac317a60aa62253e0b812
szelebodri <157057779+szelebodri@users.noreply.github.com>
2024-01-18 20:57:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from bs4 import BeautifulSoup
 
 from constants import HOME_URL, JSON_FOLDER, JSON_FILEPATH
 from scrapper import generate_urls, save_urls_to_json, get_data_game
+from clases import BannedException
 
 res = requests.get(HOME_URL)
 sopa = BeautifulSoup(res.text, 'html5lib')
 
 if __name__ == '__main__':
     if res.status_code == 429:
-        raise Exception('I am banned')
+        raise BannedException()
     links = generate_urls(HOME_URL, sopa)
     json_object = json.dumps(links, indent=4)
     save_urls_to_json(json_object, JSON_FOLDER, "links2")
